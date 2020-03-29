ec06b448fdb7b191fdc6f19447b3576f3f635c85
blazlken <azioaria@gmail.com>
2020-03-30 00:00:07 +0800

    Added Functionalities

--- Music_Player.py
+++ Music_Player.py
@@ -4,7 +4,6 @@ import threading
 import os
 import openpyxl
 
-
 class MainApplication(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
@@ -16,7 +15,7 @@ class MainApplication(tk.Frame):
         Next_Track_Btn = tk.Button(self.parent, text="Next Track", command=self.Next_Track)
         Next_Track_Btn.place(relx=0.7, rely=0.5, anchor='center')
 
-'''        Prev_Track_Btn = tk.Button(self.parent, text="Previous Track", command=self.Prev_Track)
+        Prev_Track_Btn = tk.Button(self.parent, text="Previous Track", command=self.Prev_Track)
         Prev_Track_Btn.place(relx=0.3, rely=0.5, anchor='center')
 
         Next_P_Btn = tk.Button(self.parent, text="Next Playlist", command=self.Next_Playlist)
@@ -24,39 +23,95 @@ class MainApplication(tk.Frame):
 
         Prev_P_Btn = tk.Button(self.parent, text="Previous Playlist", command=self.Prev_Playlist)
         Prev_P_Btn.place(relx=0.3, rely=0.8, anchor='center')
-'''
+
         pass
 
-    def Next_Track():
-        trackNum += 1
-        if Playlist().ActivePlaylist[]
+    def Next_Playlist(self):
+        if Playlist.playNum >= (Playlist.WSNum - 1):
+            Playlist.playNum = 0
+            pass
+        else:
+            Playlist.playNum += 1
 
+        self.ActivePlaylist = Playlist.wb.sheetnames[Playlist.playNum]
 
+        Playlist.FirstEmpty = None
+        num = 2
+        while Playlist.FirstEmpty is None:
+            if Playlist.wb[Playlist.wb.sheetnames[Playlist.playNum]]['B' + str(num)].value is None:
+                Playlist.FirstEmpty = 'B' + str(num)
+                pass
+            else:
+                num += 1
+                pass
+
+        pass
+
+    def Prev_Playlist(self):
+        if Playlist.playNum >= (Playlist.WSNum - 1):
+            Playlist.playNum = 0
+            pass
+        else:
+            Playlist.playNum += 1
+
+        self.ActivePlaylist = Playlist.wb.sheetnames[Playlist.playNum]
+
+        Playlist.FirstEmpty = None
+        num = 2
+        while Playlist.FirstEmpty is None:
+            if Playlist.wb[Playlist.wb.sheetnames[Playlist.playNum]]['B' + str(num)].value is None:
+                Playlist.FirstEmpty = 'B' + str(num)
+                pass
+            else:
+                num += 1
+                pass
+        
+        pass
+
+    def Next_Track(self):
+        Playlist.trackNum += 1
+        if ('B' + str(Playlist.trackNum)) == Playlist.FirstEmpty:
+            Playlist.trackNum = 2
+            pass
+        else:
+            pass
+
+    def Prev_Track(self):
+        Playlist.trackNum += 1
+        if ('B' + str(Playlist.trackNum)) == Playlist.FirstEmpty:
+            Playlist.trackNum = 2
+            pass
+        else:
+            pass
+
+    
 class Playlist():
     def __init__(self):
-        self.filepath = os.path.dirname(os.path.abspath(__file__)) + "\Music\Playlist.xlsx"
+        self.filepath = os.path.dirname(os.path.abspath(__file__)) + "\Music\Playlists.xlsx"
+        self.playNum = 0
+        self.trackNum = 2
 
-        wb = openpyxl.load_workbook(self.filepath,True)
+        self.wb = openpyxl.load_workbook(self.filepath,True)
         
-        self.WSNum = len(wb.sheetnames)
-        self.ActivePlaylist = wb[wb.sheetnames[self.WSNum]]
-
-        pass
-
-class Tracks():
-    def __init__(self):
+        self.WSNum = len(self.wb.sheetnames)
 
         self.FirstEmpty = None
         num = 2
         while self.FirstEmpty is None:
-            if Playlist.ActivePlaylist['B' + str(num)] is None:
+            if self.wb[self.wb.sheetnames[self.playNum]]['B' + str(num)].value is None:
                 self.FirstEmpty = 'B' + str(num)
                 pass
             else:
                 num += 1
                 pass
 
+        pass
+
+
 if __name__ == "__main__":
+    Playlist = Playlist()
+
     root = tk.Tk()
-    MainApplication(root).pack(side="top", fill="both", expand=False)
+    MainApp = MainApplication(root)
+    MainApp.pack(side="top", fill="both", expand=False)
     root.mainloop()
